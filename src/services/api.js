b26536dd6def746ba891f158cf5720bd96c79897
Richieri Negri <ruivotech@hotmail.com>
2020-03-19 17:37:12 -0300

    Merge to Dev into master

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import axios from "axios";
 
 //const URL = "http://192.168.0.4/cem-api";
@@ -28,58 +27,6 @@ const api = {
         let data = await request.delete(URL + local + "/" + dados);
 
         return data.data;
-=======
-import { NotificationManager } from 'react-notifications';
-
-const URL = "http://api.ruivotech.com.br/cem-api";
-//const URL = "http://177.154.248.249:8080/cem-api";
-
-const api = {
-    get: async function (local, dados) {
-        console.log(dados);
-        dados = dados !== undefined ? "/" +  dados : "";
-        let resultado = await fetch(URL + local + dados);
-        
-        if(resultado.status !== 200) {
-            NotificationManager.error("Algo deu errado, favor entrar em contato com o Adminsitrador do sistema!", 'Erro!');
-        }else{
-            let data = await resultado.json();
-
-            return data;
-        }
-    },
-
-    post: async function (local, dados) {
-        let resultado;
-        resultado = await fetch(URL + local, {
-            method: 'POST',
-            headers: {'Content-Type':'application/json'},
-            body: JSON.stringify(dados)
-        });
-
-        if(resultado.status !== 200) {
-            NotificationManager.error("Algo deu errado, favor entrar em contato com o Adminsitrador do sistema!", 'Error!');
-        }else{
-            let data = await resultado.json();
-
-            return data;
-        }
-    },
-
-    delete: async function (local, dados) {
-        let resultado = await fetch(URL + local + "/" + dados, {
-            method: "DELETE",
-            headers: { 'Content-Type': 'application/json' }
-        });
-
-        if(resultado.status !== 200) {
-            NotificationManager.error("Algo deu errado, favor entrar em contato com o Adminsitrador do sistema!", 'Error!');
-        }else{
-            let data = await resultado.json();
-
-            return data;
-        }
->>>>>>> Dev
     }
 }
 
