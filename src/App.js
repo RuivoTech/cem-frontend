db01e518eba81ac68c3d7828d056f5ae5139c1b7
Richieri Negri <ruivotech@hotmail.com>
2020-03-19 15:40:57 -0300

    Testes

--- src/App.js
+++ src/App.js
@@ -53,16 +53,16 @@ class App extends Component {
                         </>
                     ) : ( null )}
                     <Switch>
-                        <Route exact path="/"
+                        <Route exact path="./"
                         render={(props) => <Login {...props} autorizado={this.autorizado} />} />
-                        <PrivateRoute exact path="/home" component={Home} toggleSidebar={this.toggleSidebar}/>
-                        <PrivateRoute exact path="/membro" component={Membro} toggleSidebar={this.toggleSidebar} />
-                        <PrivateRoute exact path="/visitante" component={Visitante} toggleSidebar={this.toggleSidebar} />
-                        <PrivateRoute exact path="/ministerio" component={Ministerio} toggleSidebar={this.toggleSidebar} />
-                        <PrivateRoute exact path="/evento" component={Evento} toggleSidebar={this.toggleSidebar} />
-                        <Route exact path="/recuperar" component={Recuperar} />
-                        <Route exact path="/inscricoes" component={Inscricoes} />
-                        <Route component={NotFound} />
+                        <PrivateRoute exact path="./home" component={Home} toggleSidebar={this.toggleSidebar}/>
+                        <PrivateRoute exact path="./membro" component={Membro} toggleSidebar={this.toggleSidebar} />
+                        <PrivateRoute exact path="./visitante" component={Visitante} toggleSidebar={this.toggleSidebar} />
+                        <PrivateRoute exact path="./ministerio" component={Ministerio} toggleSidebar={this.toggleSidebar} />
+                        <PrivateRoute exact path="./evento" component={Evento} toggleSidebar={this.toggleSidebar} />
+                        <Route exact path="./recuperar" component={Recuperar} />
+                        <Route exact path="./inscricoes" component={Inscricoes} />
+                        <Route path="*" component={NotFound} />
                     </Switch>
                 </>
             </BrowserRouter>
