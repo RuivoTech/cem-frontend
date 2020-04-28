1b010c05a2ddff269db366efcad610d85b8e0501
Richieri Negri <ruivotech@hotmail.com>
2020-04-28 15:53:02 -0300

    Alterado verificação de url.
     - Removido verificação de url para não ver se está em localhost ou em produção na master.

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const hostname = window.location.hostname;
-
 const request = axios.create({
-    baseURL: hostname === "localhost" ? "http://192.168.0.7/cem-api" : "https://api.ruivotech.com.br/cem-api",
+    baseURL: "https://api.ruivotech.com.br/cem-api",
     responseType: "json"
 });
 
