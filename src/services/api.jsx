0be0cdbd38e519913087ecec598de410507120c9
Richieri Negri <ruivotech@hotmail.com>
2020-09-22 15:11:44 -0300

    Alterado host da API.

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const URL = "https://" + window.location.hostname + ":3333";
+export const URL = "https://cem-api.ruivotech.com.br:3333";
 export const URL_RELATORIO = "https://" + window.location.hostname;
 
 const api = axios.create({
