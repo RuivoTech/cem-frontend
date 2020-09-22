ea8c0d3bb4602c2c72d9fcc5779fe4971cce2d48
Richieri Negri <ruivotech@hotmail.com>
2020-09-22 15:50:13 -0300

    Updates

---
+++ precache-manifest.c03e72c8f0849516330bf8dce21f38fc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e749d948a814f013f06516e340d36b31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acf1f18c9f4906d0f8d4",
+    "url": "/static/css/2.4c1694bd.chunk.css"
+  },
+  {
+    "revision": "6929fef06ad7195a5cda",
+    "url": "/static/css/main.6c38d39c.chunk.css"
+  },
+  {
+    "revision": "acf1f18c9f4906d0f8d4",
+    "url": "/static/js/2.06b18e47.chunk.js"
+  },
+  {
+    "revision": "bc3d92b8a267d342eb7718b9a67f62b8",
+    "url": "/static/js/2.06b18e47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6929fef06ad7195a5cda",
+    "url": "/static/js/main.baacbd6d.chunk.js"
+  },
+  {
+    "revision": "a877dec21220ded1c705",
+    "url": "/static/js/runtime-main.8f1cc303.js"
+  },
+  {
+    "revision": "310e2928a34d9dadf7ddb7fa944360bb",
+    "url": "/static/media/Logo1.310e2928.jpg"
+  },
+  {
+    "revision": "445771aa6ceae8ecc02b529bb4b4d9aa",
+    "url": "/static/media/Logo2.445771aa.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
